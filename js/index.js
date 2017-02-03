9f144dfdd328eff715ff4f6f4dcf01fc0aafacef
Tom L <Tomhearsu@outlook.com>
2017-02-03 16:05:22 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$("#lw").click()
+
+$( "#lw" ).click(function() {
+  window.open("lw.html");
+});
+
+$("#rqg").click()
+
+$( "#rqg" ).click(function() {
+  window.open("rqg.html");
+});
+
+$("#wv").click()
+
+$( "#wv" ).click(function() {
+  window.open("wv.html");
+});
+$("#calc").click()
+
+$( "#calc" ).click(function() {
+  window.open("calc.html");
+});
+
+$("#twitch").click()
+
+$( "#twitch" ).click(function() {
+  window.open("twitch.html");
+});
+
+$( "#timer" ).click(function() {
+  window.open("timer.html");
+});
+
+$( "#ttt" ).click(function() {
+  window.open("tic.html");
+});
+
+$( "#simon" ).click(function() {
+  window.open("simon.html");
+});
